2ba1ebaac8a068283c19ad0a70407b7fc15bc4e7
Mustafa Paigeer <paigeer2012@gmail.com>
2022-04-13 01:09:09 +0000

    real-world model specs for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-    # before_action :set_user, only: [:show, :edit, :update, :destroy]
+   before_action :set_user, only: [:show, :edit, :update, :destroy]
   
     def new
        @user = User.new
@@ -16,29 +16,29 @@ class UsersController < ApplicationController
         end
     end
   
-    # def update
-    #   respond_to do |format|
-    #     if @user.update(user_params)
-    #       format.html { redirect_to @user, notice: 'User was successfully updated.' }
-    #     else
-    #       format.html { render :edit }
-    #     end
-    #   end
-    # end
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
+      end
+    end
   
-    # # DELETE 
-    # def destroy
-    #   @user.destroy
-    #   respond_to do |format|
-    #     format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-    #   end
-    # end
+    # DELETE 
+    def destroy
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      end
+    end
   
     private
-      # Use callbacks to share common setup or constraints between actions.
-      # def set_user
-      #   @user = User.find(params[:id])
-      # end
+      Use callbacks to share common setup or constraints between actions.
+      def set_user
+        @user = User.find(params[:id])
+      end
   
       def user_params
         params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
